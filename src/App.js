352d96168a4c6009e992071fe398526b9389f040
bcameron1489 <bcameron1489@gmail.com>
2018-09-11 13:41:07 -0400

    Create successful request to musixmatch api for top 10 tracks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Navbar from './components/layout/Navbar'
+import Index from './components/layout/Index'
+
+import { Provider } from './context'
 
 import './App.css';
 
@@ -7,9 +12,18 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar/>
-      </div>
+      <Provider>
+      <Router>
+        <React.Fragment>
+          <Navbar/>
+          <div className="container">
+            <Switch>
+              <Route exact path='/' component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    </Provider>
     );
   }
 }
